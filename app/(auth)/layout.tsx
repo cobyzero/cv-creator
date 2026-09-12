9677008bf3846d6034f25f56fba0196fbe4908ff
Sebastian Abal Salazar <115430199+cobyzero@users.noreply.github.com>
2026-09-12 09:03:14 -0500

    removed

--- app/(auth)/layout.tsx
+++
@@ -1,3 +0,0 @@
-export default function AuthRootLayout({ children }: { children: React.ReactNode }) {
-  return <div className="min-h-screen bg-white dark:bg-zinc-950">{children}</div>;
-}
--- lib/ats/index.ts
+++
@@ -1,3 +0,0 @@
-export * from "./types";
-export * from "./job-parser";
-export * from "./scorer";
